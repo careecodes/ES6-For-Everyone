805dc2091f07e0ffc79f2c787ba1cfd3a69dc271
caree claunch <caree@caree.codes>
2018-02-20 15:05:27 -0600

    Convert to arrow

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const firstNames = ['Lucas', 'Dakota', 'Dylan', 'Naomi'];
 const lastName = 'Youngman';
 const fullNames = firstNames.map(function(name) {
     return `${name} ${lastName}`;
+});
+
+const fullNames2 = firstNames.map((name) => {
+    return `${name} ${lastName}`;
 })
 
-console.log(fullNames);
+console.log(fullNames2);
