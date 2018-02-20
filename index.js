b0c5e62cc4d041117e07eb67b6db1cef60646a06
caree claunch <caree@caree.codes>
2018-02-20 15:07:41 -0600

    Implicit return

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const firstNames = ['Lucas', 'Dakota', 'Dylan', 'Naomi'];
 const lastName = 'Youngman';
+
 const fullNames = firstNames.map(function(name) {
     return `${name} ${lastName}`;
 });
 
 const fullNames2 = firstNames.map((name) => {
     return `${name} ${lastName}`;
-})
+});
 
-console.log(fullNames2);
+// Implicit return
+const fullNames3 = firstNames.map((name) => `${name} ${lastName}`);
+
+console.log(fullNames3);
