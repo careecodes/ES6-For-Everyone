422795025c53bf0d31872da36d1e5b1367fa35af
caree claunch <caree@caree.codes>
2018-02-20 15:22:15 -0600

    Adding object literal synatx in implicit returns

--- 7_MoreArrowFunctionsExamples/index.js
+++ 7_MoreArrowFunctionsExamples/index.js
@@ -1,2 +1,5 @@
 const race = '100m Dash';
 const winners = ['some name', 'another name', 'anotha name'];
+
+// wrap the object in parenthesis to show that we are implicit returning an object literal
+const win = winners.map((winner, race) => ({name: winner, race: race, place: i}));
