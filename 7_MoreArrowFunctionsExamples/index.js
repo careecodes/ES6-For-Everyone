63413df43562d896693c795d2289bdb4631890c7
caree claunch <caree@caree.codes>
2018-02-20 15:23:41 -0600

    Added notes section for video 7

--- 7_MoreArrowFunctionsExamples/index.js
+++ 7_MoreArrowFunctionsExamples/index.js
@@ -2,4 +2,15 @@ const race = '100m Dash';
 const winners = ['some name', 'another name', 'anotha name'];
 
 // wrap the object in parenthesis to show that we are implicit returning an object literal
-const win = winners.map((winner, race) => ({name: winner, race: race, place: i}));
+const win = winners.map((winner, i) => ({name: winner, race: race, place: i}));
+
+
+
+
+
+
+
+
+
+// NOTES:
+// console.table(win) in the console will return a table of objects 
